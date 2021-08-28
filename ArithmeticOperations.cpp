267cce0d32795bdc3f12c3adc9d914318c37c47e
lovethepreet <lovethepreet@gmail.com>
2021-08-28 23:52:34 +0530

    2

---
+++ ArithmeticOperations.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a and b \n";
+    cin>>a>>b;
+    cout<<"\n addition:\n";
+    cout<<a+b;
+    cout<<"\n subtraction: \n";
+    cout<<a-b;
+    cout<<"\n multiplication: \n";
+    cout<<a*b;
+    cout<<"\n division \n";
+    cout<<a/b;
+    cout<<"\n modulus \n";
+    cout<<a%b;
+
+    return 0;
+}
